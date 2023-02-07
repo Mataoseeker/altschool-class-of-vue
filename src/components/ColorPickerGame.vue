c664f65f4155acdff5b43781e9bd926e3ada463c
Martha Osika <mataoseeker@gmail.com>
2023-02-07 11:57:44 +0100

    commit message

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,36 +1,35 @@
 <template>
   <h1>Color Picker Game</h1>
+<br />
 
-  <div>{{ message }}</div>
-
+  <div class="msg">{{ message }}</div>
+<br />
   <button v-for="color in colors" :key="color" @click="matchColor(color)">
     {{ color }}
   </button>
+
 </template>
 
-<script>
-import { ref, reactive } from "@vue/reactivity";
-
-export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
-  },
-};
+<script setup>
+import useColorPickerGame from "../composables/ColorPickerGame";
+const { colors, message, matchColor } = useColorPickerGame();
 </script>
 
-<style></style>
+<style>
+.msg{
+  font-size: 1.0rem;
+  font-weight: bold;
+}
+h1{
+  color: blueviolet;
+}
+button{
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
+  background-color: rgb(46, 42, 42);
+  color: White;
+  font-size: 1.2rem;  
+}
+</style>
